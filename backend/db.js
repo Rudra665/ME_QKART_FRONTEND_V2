1ddd0180c503c3b140b68a90be59549e110e9ccb
Rudra665 <bhadouriarudra665@gmail.com>
2026-05-23 23:58:53 +0530

    fix: update nedb import and ensure consistent module exports in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
-const nedb = require('nedb');
-const users = new nedb({ filename: 'db/users.db', autoload: true })
-const products = new nedb({ filename: 'db/products.db', autoload: true });
+const nedb = require("@seald-io/nedb");
+const users = new nedb({ filename: "db/users.db", autoload: true });
+const products = new nedb({ filename: "db/products.db", autoload: true });
 
-module.exports.users = users
-module.exports.products = products
+module.exports.users = users;
+module.exports.products = products;
